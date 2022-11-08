7b918c267ec3fe88fa14fad0e505ef21101a048b
Mark Siebald <102825140+SiebaldMark93@users.noreply.github.com>
2022-11-08 23:50:37 +0000

    learning len function to count the number of characters in a string

--- main.py
+++ main.py
@@ -2,4 +2,7 @@
 print("Hello world!\nHello World!\nHello World!")
 print("Hello" + " " + "Mark!")
 
+# input() will get user input in console
+# Then print() will print the word "hello" and the user input
 print("Hello " + input("What is your name?"))
+print(len(input("What is your name?")))
